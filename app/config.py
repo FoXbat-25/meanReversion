6421291de47a8fdef5fb631e0343d21b1dd25099
FoXbat-25 <dhruvkhatri9275@gmail.com>
2025-04-27 11:58:50 +0000

    2.0

--- app/config.py
+++ app/config.py
@@ -1,3 +1,11 @@
 from decouple import config
 
 SQL_ALCHEMY_CONN=config('SQL_ALCHEMY_CONN')
+SQL_POSTGRES_CONN = config('SQL_POSTGRES_CONN')
+DB_CONFIG = {
+    'dbname': config('DB_NAME'),
+    'user': config('DB_USER'),
+    'password': config('DB_PASSWORD'),
+    'host': config('DB_HOST'),
+    'port': int(config('DB_PORT'))
+}
--- app/mean_reversion.py
+++ app/mean_reversion.py
@@ -1,8 +1,8 @@
 import sys
 sys.path.append('/home/sierra1/projects/meanReversion')
-from config import SQL_ALCHEMY_CONN
+from config import SQL_ALCHEMY_CONN, SQL_POSTGRES_CONN
 
-from ta.trend import ADXIndicator
+import psycopg2
 
 import pandas as pd 
 from sqlalchemy import create_engine
@@ -12,8 +12,8 @@ from datetime import datetime, timedelta
 
 #Bollinger Band - 2 Std, 20d SMA
 #ADX - 15d
-#Di_diff - 
-
+#Di_diff - 2 std, 20d SMA
+#ATR - 14d sma
 
 engine = create_engine(SQL_ALCHEMY_CONN)
 buy_reco = set()
@@ -74,7 +74,7 @@ def calc_adx(df, window=15): #Propritory method
         abs(df['high'] - df['close'].shift(1)),
         abs(df['low'] - df['close'].shift(1))
     ])
-
+    df['ATR'] = df['true_range'].rolling(window=14, min_periods=1).mean()
     # Wilder’s smoothing with EWM
     df['TR_smoothed'] = df['TR'].ewm(alpha=1/window, adjust=False).mean()
     df['+DM_smoothed'] = df['+DM'].ewm(alpha=1/window, adjust=False).mean()
@@ -124,48 +124,95 @@ def calc_di_diff(df):
 
 def signals(df):
     df['Strong_Buy'] = (
-        (df['20D_Z_SCORE_CLOSE'] <= -2.1) | 
+        (df['20d_z_score_close'] <= -2.1) | 
         (
             (df['di_diff_20D_zscore'] <= -2) & 
-            (df['20D_Z_SCORE_CLOSE'] <= -2)
+            (df['20d_z_score_close'] <= -2)
         )
     )
     df['Strong_Sell'] = (
-        (df['20D_Z_SCORE_OPEN'] >= 2.2) | 
+        (df['20d_z_score_open'] >= 2.2) | 
         (
             (df['di_diff_20D_zscore'] >= 2) & 
-            (df['20D_Z_SCORE_OPEN'] >=2)
+            (df['20d_z_score_open'] >=2)
         )
     )
 
     return df
 
-def trade_book(depth = 120): # How long back you want look, 
-    # this will book trades for that period and start recording open positions from next day onwards.  
+    
+def trade_book(df, depth = 130): # How long back you want look, 
+    
+    next_day_query = """
+        SELECT DATE FROM CALENDAR WHERE DATE > CURRENT_DATE AND HOLIDAY = FALSE ORDER BY DATE ASC LIMIT 1;    
+    """
+
+    entry_insert_query = f"""
+        INSERT INTO TRADE_BOOK (symbol, entry_date, entry_price, status, strategy, created_at, updated_at)
+        VALUES %s
+        ON CONFLICT (symbol, entry_date)
+        DO NOTHING
+    """
+    exit_insert_query = f"""
+        UPDATE TRADE_BOOK
+        SET exit_date = %s, exit_price = %s, updated_at = %s, status = 'closed'
+        WHERE symbol = %s AND status = 'open'
+    """
+
     latest_date = df['date'].max()
     cutoff_date = latest_date - timedelta(days=depth)
-
     recent_data = df[df['date'] >= cutoff_date]
     
     symbols = recent_data['symbol'].unique()
-    trades = []
-    open_positions = []
+
+    next_day = pd.read_sql(next_day_query, engine).iloc[0,0]
+    df['next_open'] = df.groupby('symbol')['open'].shift(-1)
+
+    conn = psycopg2.connect(SQL_POSTGRES_CONN)
+    cursor = conn.cursor()
 
     for symbol in symbols:
         symbol_df = recent_data[recent_data['symbol'] == symbol].sort_values('date', ascending = True)
+        open_positions = []
         for _, row in symbol_df.iterrows():
+            
             if not open_positions and row ['Strong_Buy'] == 1 and row['date'] < latest_date:
-                trade = {
-                    'symbol' : symbol,
-                    'entry_date' : row['date'],
-                    'entry_price' : row['next_open']
-                }
-                trades.append(trade)
-                open_positions.append(trade)
-            elif open_positions and row ['Strong_Sell'] == 1 and row['date'] < latest_date:
-                trade.update({'exit_price' : row['next_open']})
-                open_positions.pop()
+                now_timestamp = datetime.now().timestamp()
+                
+                entry_trade_data = [row['symbol'],
+                              next_day, 
+                              row['next_open'], 
+                              'open', 
+                              'Mean Reversion', 
+                              now_timestamp, 
+                              now_timestamp ]
+                
+                cursor.execute(entry_insert_query, tuple(entry_trade_data))
+                open_positions.append(next_day)
 
+                    
+            elif open_positions and row ['Strong_Sell'] == 1 and row['date'] < latest_date:
+                now_timestamp = datetime.now().timestamp()
+
+                exit_trade_data = [next_day,
+                              row['next_open'],
+                              now_timestamp,
+                              row['symbol']]
+                
+                cursor.execute(exit_insert_query, tuple(exit_trade_data))
+                open_positions = False
+
+            elif open_positions and (
+                row['z'] ==1 and row['close'] <= 0.98*recent_data[recent_data['date'] == next_day]['close']
+                |
+                row['close'] <= 0.975*recent_data[recent_data['date']==next_day]['close']
+            ):
+                stop_loss_exit = []
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    return df
 
         #Function definitions end here ----------------------------------
 df = fetch_nse_data()
@@ -176,6 +223,8 @@ df = df.merge(adx_prop, on=['symbol', 'date'], how='left')
 #setting date time format
 df['date'] = pd.to_datetime(df['date'])
 df['next_open'] = df.groupby('symbol')['open'].shift(-1)
+
+
  
 
 df = df.sort_values(by=['symbol', 'date'], ascending=[True, True])  # Very important!
@@ -187,7 +236,7 @@ df = df.sort_values(by=['symbol', 'date'], ascending=[True, False])
 print(df[df["symbol"] == "AFIL"].head(15))
 df_first_row=df.groupby('symbol').head(1)
 # print(df_first_row)
-# print(f'BUY - {strong_buy_symbols}')
+# print(f'BUY - {strong_buy_symbols}') 
 # print(f'SELL - {strong_sell_symbols}')
 
 
